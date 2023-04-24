573c1f49f0976523fc8ab58d94824bcf4d70eee3
salgu1998 <salgu1998@gmail.com>
2023-04-24 20:56:46 +0900

    log util add

---
+++ src/main/java/MissingBackPressureException.java
@@ -0,0 +1,15 @@
+import io.reactivex.Flowable;
+
+import java.util.concurrent.TimeUnit;
+
+public class MissingBackPressureException {
+    public static void main(String[] args) {
+//        Flowable.interval(1L, TimeUnit.MILLISECONDS)
+//                .doOnNext(data -> System.out.println(data))
+//                .observeOn(
+//                        data -> {
+//                            System.out.println("소비자 처리 대기중..");
+//                        }
+//                )
+    }
+}
