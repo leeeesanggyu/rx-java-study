42db44bcd1aee866e422f180248689ea4bf3b4cb
salgu1998 <salgu1998@gmail.com>
2023-04-29 00:18:06 +0900

    MissingBackPressureException example

--- src/main/java/MissingBackPressureException.java
+++ src/main/java/MissingBackPressureException.java
@@ -1,15 +1,26 @@
 import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
+import log.LogType;
+import log.Logger;
+import log.TimeUtil;
 
 import java.util.concurrent.TimeUnit;
 
 public class MissingBackPressureException {
-    public static void main(String[] args) {
-//        Flowable.interval(1L, TimeUnit.MILLISECONDS)
-//                .doOnNext(data -> System.out.println(data))
-//                .observeOn(
-//                        data -> {
-//                            System.out.println("소비자 처리 대기중..");
-//                        }
-//                )
+    public static void main(String[] args) throws InterruptedException {
+        Flowable.interval(1L, TimeUnit.MILLISECONDS)
+                .doOnNext(data -> Logger.log(LogType.DO_ON_NEXT, data))
+                .observeOn(Schedulers.computation())
+                .subscribe(
+                        data -> {
+                            Logger.log(LogType.PRINT, "# 소비자 처리 대기 중..");
+                            TimeUtil.sleep(1000L);
+                            Logger.log(LogType.ON_NEXT, data);
+                        },
+                        error -> Logger.log(LogType.ON_ERROR, error),
+                        () -> Logger.log(LogType.ON_COMPLETE)
+                );
+
+        Thread.sleep(2000L);
     }
 }
