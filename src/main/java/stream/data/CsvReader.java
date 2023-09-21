a51a58219a258a8c03effbf77af9b171040bc24c
salgu1998 <salgu1998@gmail.com>
2023-09-22 01:16:29 +0900

    csv reader 구현

---
+++ src/main/java/stream/data/CsvReader.java
@@ -0,0 +1,30 @@
+package stream.data;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvReader {
+
+    public static List<List<String>> read() {
+        String csvFile = "./src/main/java/stream/data/data.csv"; // 실제 파일 경로로 변경해주세요.
+        String line = "";
+        String csvDelimiter = ",";
+
+        List<List<String>> list = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            while ((line = br.readLine()) != null) {
+                // CSV 파일에서 한 줄을 읽고 콤마로 분리
+                String[] data = line.split(csvDelimiter);
+
+                list.add(List.of(data));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+}
