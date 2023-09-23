9255e486210beae020b16dbfa93aa982a9ce05e8
salgu1998 <salgu1998@gmail.com>
2023-09-23 14:52:09 +0900

    취미별 '새'씨 성을 갖는 멤버 수를 구하라

--- src/main/java/stream/FirstNameHobby.java
+++ src/main/java/stream/FirstNameHobby.java
@@ -5,27 +5,21 @@ import stream.data.CsvReader;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
-// 취미별 인원 수를 구하라
-public class HobbyCount {
-
+// 취미별 '새'씨 성을 갖는 멤버 수를 구하라
+public class FirstNameHobby {
     public static void main(String[] args) {
+        final String firstName = "죠";
+
         List<List<String>> datas = CsvReader.read();
         datas.remove(0);
         System.out.println("data =>" + datas);
 
         HashMap<String, Integer> resultMap = new HashMap<>();
         datas.stream()
+                .filter(data -> data.get(0).startsWith(firstName))
                 .flatMap(data -> Arrays.stream(data.get(1).trim().split(":")))
                 .forEach(hobby -> resultMap.merge(hobby, 1, (oldValue, newValue) -> ++oldValue));
-
-//        datas.stream()
-//                .flatMap(data -> Arrays.stream(data.get(1).trim().split(":")))
-//                .forEach(hobby -> {
-//                    resultMap.put(hobby, resultMap.containsKey(hobby) ? resultMap.get(hobby) + 1 : 1);
-//                });
 
         resultMap.entrySet().forEach(System.out::println);
     }
