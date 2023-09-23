5e849c45cab599bda4c005a6f0e626f84721bb64
salgu1998 <salgu1998@gmail.com>
2023-09-23 15:45:56 +0900

    각 사람이 가장 좋아하는 취미를 찾아 출력하세요.

---
+++ src/main/java/stream/MostHobby.java
@@ -0,0 +1,26 @@
+package stream;
+
+import stream.data.CsvReader;
+
+import java.util.*;
+
+// 각 사람이 가장 좋아하는 취미를 찾아 출력하세요.
+public class MostHobby {
+    public static void main(String[] args) {
+        List<List<String>> datas = CsvReader.read();
+        datas.remove(0);
+        System.out.println("data =>" + datas);
+
+        HashMap<String, Integer> hobbyCountMap = new HashMap<>();
+
+        datas.stream()
+                .flatMap(data -> Arrays.stream(data.get(1).trim().split(":")))
+                .forEach(hobby -> hobbyCountMap.merge(hobby, 1, (oldValue, newValue) -> ++oldValue));
+        System.out.println("result => " + hobbyCountMap);
+
+        String key = hobbyCountMap.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .get().getKey();
+        System.out.println("result => " + key);
+    }
+}
