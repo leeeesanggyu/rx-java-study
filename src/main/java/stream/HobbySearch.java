2816255dce132d14d2653819a21242953196ea2b
salgu1998 <salgu1998@gmail.com>
2023-09-23 16:28:07 +0900

    취미가 농구 또는 축구인 사람들을 출력하세요.

---
+++ src/main/java/stream/HobbySearch.java
@@ -0,0 +1,19 @@
+package stream;
+
+import stream.data.CsvReader;
+
+import java.util.Arrays;
+import java.util.List;
+
+// 취미가 "농구" 또는 "축구"인 사람들을 출력하세요.
+public class HobbySearch {
+    public static void main(String[] args) {
+        List<List<String>> datas = CsvReader.read();
+        datas.remove(0);
+        System.out.println("data =>" + datas);
+
+        datas.stream()
+                .filter(data -> (data.get(1).contains("농구") || data.get(1).contains("축구")))
+                .forEach(data -> System.out.println(data.get(0)));
+    }
+}
